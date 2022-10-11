a718201a45bea6ce1212a8119a3608ba792544d3
raihan512 <raihangazi1024@Gmail.com>
2022-10-12 00:50:10 +0600

    readme file updated

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # b6-quiz-crackerz-raihan512
 Project Name: Web-Dev-Quiz
+
+ Markup : 1. User can take a test on site by choosing their interest
+          2. If user choose right ans or worng then they can see that in a alert box
--- src/component/Blog/Blog.js
+++ src/component/Blog/Blog.js
@@ -3,7 +3,18 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            <h2>Blog</h2>
+            <h2 className='text-6xl font-bold'>Questions</h2>
+            <div>
+                <div className='bg-cyan-600 mx-10 my-5 p-10 rounded-md text-left'>
+                    <h5 className='2xl text-white font-semibold'>Question -1: What is the purpose of react router</h5>
+                </div>
+                <div className='bg-cyan-600 mx-10 my-5 p-10 rounded-md text-left'>
+                    <h5 className='2xl text-white font-semibold'>Question -2: How does context api works</h5>
+                </div>
+                <div className='bg-cyan-600 mx-10 my-5 p-10 rounded-md text-left'>
+                    <h5 className='2xl text-white font-semibold'>Question -3: useref hook ans</h5>
+                </div>
+            </div>
         </div>
     );
 };
