70e8b16a3419a91ee0abe5b13660e1eb834a48a6
raihan512 <raihangazi1024@gmail.com>
2022-10-12 04:36:49 -0700

    Toast added for showing correct ans and showing user item choosed result

--- src/component/Option/Option.js
+++ src/component/Option/Option.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 const Option = ({ option, getSelectedOption, quizId, message }) => {
     return (
-        <div
+        <div>
+            <div
             onClick={() => getSelectedOption(quizId, option)}
             className="bg-black border-2 rounded-md flex justify-center items-center p-5"
         >
             <input type="radio" name={quizId} id={option} className='cursor-pointer' />
             <label className='ml-2 text-lg text-white font-semibold cursor-pointer' htmlFor={option}>{option}</label>
         </div>
+        </div>
+
     );
 };
 
--- src/component/Quiz/Quiz.js
+++ src/component/Quiz/Quiz.js
@@ -1,54 +1,48 @@
 import React, { useState } from 'react';
 import Option from '../Option/Option';
 import { EyeIcon } from '@heroicons/react/24/solid'
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 
 
 
 const Quiz = ({ quiz }) => {
 
     const { question, options, correctAnswer, id } = quiz;
+    // Remove p and &nbsp; tag from question 
     const onlyQuestion = (question.slice(3, (question.length - 4))).split('&nbsp;').join(' ');
-    const [message, setMessage] = useState(' ');
-    const [correctAns, setCorrectAns] = useState(' ')
-    const showCorrectAns = (clickedCorrecctAns) => {
-        setCorrectAns(clickedCorrecctAns)
-    }
 
+    // Show correct answer in a toast when click on eye icon 
+    const notify = (clickedCorrecctAns) => {
+        toast(`Correct answer is --- ${clickedCorrecctAns}`)
+    };
+    // If selected item is right or wrong show the result to the user by a toast
     const getSelectedOption = (optionId, optionName) => {
-        // const selectedAnsArray = []
         const selectedAns = { optionId: optionName }
         const givenCorrectAns = { id: correctAnswer };
-        // selectedAnsArray.push(selectedAns);
 
         if (selectedAns.optionId === givenCorrectAns.id) {
-            // alert('Your answer is right')
-            setMessage('correct')
+            toast('Your anser is Right');
         } else if (selectedAns.optionId !== givenCorrectAns.id) {
-            setMessage('wrong')
-            // alert('Your anser is wrong')
+            toast('Your anser is Wrong');
         }
-        else {
-            setMessage(' ')
-        }
-        return message
-
     }
     return (
         <div className='bg-black mb-5 mx-5 md:mx-20 rounded-md py-5 relative'>
             <button
                 className='absolute top-5 right-5'
-                onClick={() => showCorrectAns(correctAnswer)}
+                onClick={()=> notify(correctAnswer)}
             >
                 <div className='flex items-center'>
                     <span className='mr-2 text-xl text-red-700'>Show Correct Answer</span>
                     <EyeIcon className="h-6 w-6 text-red-700" />
                 </div>
             </button>
+            <ToastContainer />
             <div className='text-xl font-bold m-8 text-white'>
                 {
                     onlyQuestion
                 }
-
             </div>
             <div>
                 <form action="" className='grid grid-cols-1 md:grid-cols-2 gap-5 m-5'>
@@ -61,13 +55,6 @@ const Quiz = ({ quiz }) => {
                         ></Option>)
                     }
                 </form>
-                <p className='text-green-600 text-xl font-semibold'>{correctAns}</p>
-                {
-                    message === 'correct' && <p className='text-lime-500 text-2xl font-semibold'>Right Answer</p>
-                }
-                {
-                    message === 'wrong' && <p className='text-red-700 text-2xl font-semibold'>Wrong answer</p>
-                }
             </div>
         </div>
     );
